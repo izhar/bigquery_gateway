dd70d4d9e88b919cace458fe37d1944ada6a0bd8
izhar <izharravid@gmail.com>
2019-05-08 19:49:18 +0300

    added schema file import

--- lib/bigquery_gateway/utils.ex
+++ lib/bigquery_gateway/utils.ex
@@ -1,4 +1,39 @@
 defmodule BigqueryGateway.Utils do
+
+  @doc """
+  Convert map string keys to :atom keys
+  """
+  def atomize_keys(nil), do: nil
+
+  # Structs don't do enumerable and anyway the keys are already
+  # atoms
+  def atomize_keys(struct = %{__struct__: _}) do
+    struct
+  end
+
+  def atomize_keys(map = %{}) do
+    map
+    |> Enum.map(fn {k, v} -> {String.to_atom(k), atomize_keys(v)} end)
+    |> Enum.into(%{})
+  end
+
+  # Walk the list and atomize the keys of
+  # of any map members
+  def atomize_keys([head | rest]) do
+    [atomize_keys(head) | atomize_keys(rest)]
+  end
+
+  def atomize_keys(not_a_map) do
+    not_a_map
+  end
+
+  def schema_from_file(file_path) do
+    #File.read!("./priv/historical_schema.json") |> Poison.decode!()
+    file_path = "./priv/historical_schema.json"
+    File.read!(file_path) |> Poison.decode!()
+    |> atomize_keys()
+  end
+
   @doc """
   Create a http friendly nonce
 
